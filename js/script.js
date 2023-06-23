6f05730991cdd40cf16b3e0709c35ee1b81238c6
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-06-23 16:23:04 +0000

    recuperer les information page accueil

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const ipt = document.querySelector(".ipt")
+const depart = document.querySelector("#depart")
+const arriver = document.querySelector("#arriver")
+const date = document.querySelector("#date")
+const heure = document.querySelector("#heure")
+const soumettre = document.querySelector('.soumettre')
+
+
+soumettre.addEventListener("click",(event) =>{
+    event.preventDefault();
+    console.log(ipt.value);
+    console.log(depart.value);
+    console.log(arriver.value);
+    console.log(date.value);
+    console.log(heure.value);
+
+    const monTicket = {
+        nom:ipt.value,
+        mondepart:depart.value,
+        monarriver:arriver.value,
+        madate:date.value,
+        monheure:heure.value
+    };
+
+    localStorage.setItem("monTicket", JSON.stringify(monTicket))
+    window.location.href="/html/verif.html"; 
+})
--- js/verif.js
+++ js/verif.js
@@ -1,17 +1,49 @@
 const modalContainer = document.querySelector(".modal-container")
 const modalTriggers = document.querySelectorAll(".modal-trigger")
-const btn = document.querySelector(".boutton")
-const bttn = document.querySelector(".bouton")
-console.log(btn);
+const bancaire = document.querySelector(".bancaire")
+const money = document.querySelector(".money")
+const valider = document.querySelector(".valider")
+const liens = document.querySelector(".liens")
+const lien = document.querySelector(".lien")
+
+const myname = document.querySelector(".myname")
+const mydepart = document.querySelector(".mydepart")
+const myarriver = document.querySelector(".myarriver")
+const mydate = document.querySelector(".mydate")
+const myheure = document.querySelector(".myheure")
+
+
 
 modalTriggers.forEach(trigger => trigger.addEventListener("click",toggleModal));
 
 function toggleModal(){
     modalContainer.classList.toggle("active")
 }
-btn.addEventListener("click", (e)=>{
+bancaire.addEventListener("click", (e)=>{
     e.preventDefault()
 })
-bttn.addEventListener("click", (e)=>{
+money.addEventListener("click", (e)=>{
     e.preventDefault()
 })
+valider.addEventListener("click", (e)=>{
+    e.preventDefault();
+    window.location.pathname="/html/ticket.html";
+})
+liens.addEventListener("click", (e)=>{
+    e.preventDefault();
+    window.location.href="/index.html";
+})
+lien.addEventListener("click", (e)=>{
+    e.preventDefault();
+    window.location.href="/index.html";
+})
+let recuperer = localStorage.getItem("monTicket")
+
+
+recuperer = JSON.parse(recuperer)
+myname.textContent=recuperer.nom
+mydepart.textContent=recuperer.mondepart
+myarriver.textContent=recuperer.monarriver
+mydate.textContent=recuperer.madate
+myheure.textContent=recuperer.monheure
+console.log(recuperer.mondepart)
